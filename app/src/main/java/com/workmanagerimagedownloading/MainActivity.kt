2025ba190ff80e3f060fa8799dd1656665a2f5fd
Ahmed <ahmedchoteri@gmail.com>
2024-05-16 01:45:04 +0530

    removed dependencies

--- app/src/main/java/com/workmanagerimagedownloading/MainActivity.kt
+++ app/src/main/java/com/workmanagerimagedownloading/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
 
